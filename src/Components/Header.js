737c9d33e75100ceff2f2aecdeeb50957b035c6d
Maria McGovern <mtgovern@me.com>
2020-10-01 17:37:58 -0400

    update copy

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,6 @@ class Header extends Component {
       var name = this.props.data.name;
       var occupation= this.props.data.occupation;
       var description= this.props.data.description;
-      // var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -26,8 +25,6 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Skills</a></li>
             <li><a className="smoothscroll" href="#portfolio">Projects</a></li>
-            {/* <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li> */}
-            {/* <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
          </ul>
 
       </nav>
